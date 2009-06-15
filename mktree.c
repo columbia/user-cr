24f69887a07b355b93ec51b7df80e719c783f90b
Serge E. Hallyn <serue@us.ibm.com>
2009-06-15 14:08:21 -0500

    mktree: use clone_with_pid on s390
    Signed-off-by: Serge Hallyn <serue@us.ibm.com>
    [orenl@cs.columbia.edu: don't define __NR_clone_with_pids]

--- mktree.c
+++ mktree.c
@@ -1691,9 +1691,14 @@ static int ckpt_write_tree(struct ckpt_ctx *ctx)
 	return 0;
 }
 
-#if defined(__i386__) && defined(__NR_clone_with_pids)
 /*
  * libc doesn't support clone_with_pid() yet...
+ * below is arch-dependent code to use the syscall
+ */
+#if defined(__i386__) && defined(__NR_clone_with_pids)
+
+/*
+ * x86_32
  * (see: http://lkml.indiana.edu/hypermail/linux/kernel/9604.3/0204.html)
  */
 static int clone_with_pids(int (*fn)(void *), void *child_stack, int flags,
@@ -1745,16 +1750,56 @@ static int clone_with_pids(int (*fn)(void *), void *child_stack, int flags,
 	}
 	return retval;
 }
-#else
+
+#elif defined(__s390__) && defined(__NR_clone_with_pids)
+
 /*
- * libc doesn't support clone_with_pid() yet...
- * on other architectures fallback to regular clone(2)
+ * s390
  */
+#define do_clone_with_pids(stack, flags, ptid, ctid, setp) ({ \
+	register unsigned long int __r2 asm ("2") = (unsigned long int)(stack);\
+	register unsigned long int __r3 asm ("3") = (unsigned long int)(flags);\
+	register unsigned long int __r4 asm ("4") = (unsigned long int)(ptid); \
+	register unsigned long int __r5 asm ("5") = (unsigned long int)(ctid); \
+	register unsigned long int __r6 asm ("6") = (unsigned long int)(NULL); \
+	register unsigned long int __r7 asm ("7") = (unsigned long int)(setp); \
+	register unsigned long int __result asm ("2"); \
+	__asm__ __volatile__( \
+		" lghi %%r1,332\n" \
+		" svc 0\n" \
+		: "=d" (__result) \
+		: "0" (__r2), "d" (__r3), \
+		  "d" (__r4), "d" (__r5), "d" (__r6), "d" (__r7) \
+		: "1", "cc", "memory" \
+	); \
+		__result; \
+	})
+
+int clone_with_pids(int (*fn)(void *), void *child_stack, int flags,
+			struct target_pid_set *target_pids, void *arg)
+{
+	long retval;
+	retval = do_clone_with_pids(child_stack, flags, NULL, NULL,
+				    target_pids);
+
+	if (retval < 0) {
+		errno = -retval;
+		return -1;
+	} else if (retval == 0) {
+		return fn(arg);
+	} else
+		return retval;
+}
+
+#else  /* !defined(__NR_clone_with_pids) */
+
+/* on other architectures fallback to regular clone(2) */
 static int clone_with_pids(int (*fn)(void *), void *child_stack, int flags,
 			   struct target_pid_set *target_pids, void *arg)
 {
 	return clone(fn, child_stack, flags, arg);
 }
+
 #endif
 
 /*
