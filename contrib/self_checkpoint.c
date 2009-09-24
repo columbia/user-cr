50c846b3d75b3a3e33900c615cfa5ffece2d03b4
Oren Laadan <orenl@cs.columbia.edu>
2009-09-24 01:58:13 -0400

    Move self_{checkpoint,restart}.c examlpe code to contrib/
    As suggested by Matt Helsley.
    
    Signed-off-by: Oren Laadan <orenl@cs.columbia.edu>

--- contrib/self_checkpoint.c
+++ contrib/self_checkpoint.c
@@ -18,6 +18,11 @@
 
 #include <linux/checkpoint.h>
 
+inline static int checkpoint(pid_t pid, int fd, unsigned long flags)
+{
+	return syscall(__NR_checkpoint, pid, fd, flags);
+}
+
 #define OUTFILE  "/tmp/cr-self.out"
 
 int main(int argc, char *argv[])
@@ -25,7 +30,6 @@ int main(int argc, char *argv[])
 	pid_t pid = getpid();
 	FILE *file;
 	int i, ret;
-	float a;
 
 	close(0);
 	close(2);
@@ -41,28 +45,24 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	a = sqrt(2.53 * (getpid() / 1.21));
-
 	fprintf(file, "hello, world (%.2f)!\n", a);
 	fflush(file);
 
 	for (i = 0; i < 1000; i++) {
 		sleep(1);
-		/* make the fpu work ->  a = a + i/10  */
-		a = sqrt(a*a + 2*a*(i/10.0) + i*i/100.0);
-		fprintf(file, "count %d (%.2f)!\n", i, a);
+		fprintf(file, "count %d\n", i);
 		fflush(file);
 
-		if (i == 2) {
-			ret = syscall(__NR_checkpoint,
-				      pid, STDOUT_FILENO, CHECKPOINT_SUBTREE);
-			if (ret < 0) {
-				fprintf(file, "ckpt: %s\n", strerror(errno));
-				exit(2);
-			}
-			fprintf(file, "checkpoint ret: %d\n", ret);
-			fflush(file);
+		if (i != 2)
+			continue;
+		ret = checkpoint(pid, STDOUT_FILENO, CHECKPOINT_SUBTREE);
+		if (ret < 0) {
+			fprintf(file, "ckpt: %s\n", strerror(errno));
+			exit(2);
 		}
+
+		fprintf(file, "checkpoint ret: %d\n", ret);
+		fflush(file);
 	}
 		
 	return 0;
