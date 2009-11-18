49c2a25334c72d419088a05ffd866eeb8fbd9cb8
Nathan Lynch <ntl@pobox.com>
2009-11-17 18:55:46 -0600

    user-cr: remove powerpc clone_with_pids
    Remove C and asm support code for old clone_with_pids interface and
    update comments accordingly.
    
    Signed-off-by: Nathan Lynch <ntl@pobox.com>

--- clone_ppc.c
+++ clone_ppc.c
@@ -16,46 +16,11 @@
 #include <sys/syscall.h>
 #include <asm/unistd.h>
 
-struct target_pid_set;
-
-extern int __clone_with_pids(int (*fn)(void *arg),
-			     void *child_stack ,
-			     int flags,
-			     void *arg,
-			     void *parent_tid,
-			     void *tls,
-			     void *child_tid,
-			     struct target_pid_set *setp);
-
 /*
- * libc doesn't support clone_with_pid() yet...
+ * libc doesn't support eclone() yet...
  * below is arch-dependent code to use the syscall
  */
 #include <linux/checkpoint.h>
-#if defined(__NR_clone_with_pids)
-
-/* (see: http://lkml.indiana.edu/hypermail/linux/kernel/9604.3/0204.html) */
-
-int clone_with_pids(int (*fn)(void *), void *child_stack, int flags,
-			   struct target_pid_set *target_pids, void *arg)
-{
-	void *parent_tid = NULL;
-	void *tls = NULL;
-	void *child_tid = NULL;
-	pid_t newpid;
-
-	newpid = __clone_with_pids(fn, child_stack, flags, arg, parent_tid,
-				   tls, child_tid, target_pids);
-
-	if (newpid < 0) {
-		errno = -newpid;
-		return -1;
-	}
-
-	return newpid;
-}
-
-#endif
 
 #include "eclone.h"
 
